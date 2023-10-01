5f30048e9f3d6db77ed69940be29ecc3720653fb
chloebweaver <132216902+chloebweaver@users.noreply.github.com>
2023-10-01 12:00:34 -0500

    Add instructions as comments to rps.js

---
+++ rps.js
@@ -0,0 +1,25 @@
+// Create a function called getComputerChoice
+// that will randomly return r, p, or s.
+
+
+
+// Write a function that allows a single round of rps.
+// It should take two parameters,
+// playerSelection and computerSelection,
+// and return a string declaring a winner:
+// "You lose! Paper beats rock."
+// The playerSelection should be case-insensitive.
+
+
+
+// console.log the returned string to test the code.
+// Then remove the console.log portion of code.
+
+
+
+// Write a new function called game().
+// Either loop your previous function or
+// call it five times.
+// NOW you may console.log the winner declaration.
+// Use prompt() to get user input.
+// Feel free to create more helper functions for utility.
